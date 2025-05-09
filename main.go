d27f17a2ab32cf65110e4fbf8fdddc17098161aa
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-09 19:53:22 +0300

    feat: update main.go

--- main.go
+++ main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hellooo")
+	books := [10]string{
+		"Adam Zaber, “Calculus A”",
+		"Bruce Yonder, “Calculus B”",
+		"Clive Saber, “Linear Algebra”",
+		"David Faber, “Probability Theory”",
+		"Eric Tander, “Discrete Mathematics”",
+		"Fred Pater, “Statistics”",
+		"Greg Norer, “Algorithms and Complexity” ",
+		"Hugh Qatar, “Languages and Machines”",
+		"Ian Mirror, “Algorithmic Discrete Mathematics”",
+		"Jack Loner, “Graph Isomorphisms”",
+	}
+
+	backtrack(books[0:10])
+}
+
+func backtrack(v []string) {
+	length := len(v)
+	middle := length / 2
+
+	if length > 1 {
+		left := v[0:middle]
+		right := v[middle:length]
+
+		backtrack(left)
+		backtrack(right)
+	} else {
+		fmt.Println(v)
+	}
 }
